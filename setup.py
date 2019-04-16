e9056c800825eb65f8e2326a3ad2dd6f2fd69eba
Renat Sibgatulin <renat.sibgatulin@gmail.com>
2019-04-16 17:56:46 +0200

    updated url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='good_bad_questionable',
       version='0.1',
       description='Demo repo for good, bad and questionable coding practices',
-#      url='https://github.com/ismrm-coding-secret-session/good-bad-questionable',
+      url='https://github.com/ismrm-coding-secret-session/good-bad-questionable',
       author='Renat Sibgatulin',
       author_email='renat.sibgatulin@gmail.com',
       license='MIT',
